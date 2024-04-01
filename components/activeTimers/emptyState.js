6f0d1f55eb8db02f2574bfcea7b7701f7b65425c
Maverick Seneris <maverickseneris@gmail.com>
2024-04-01 15:57:04 +0800

    feat: Add empty state component to ActiveTimers
    Introduce an empty state component in the ActiveTimers screen to improve user experience when there are no active timers present.

--- components/activeTimers/emptyState.js
+++ components/activeTimers/emptyState.js
@@ -2,11 +2,12 @@ import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import React from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Colors } from "../../styles/theme/Colors";
+import Card from "../shared/card";
 
-export default function EmptyState({navigation}) {
-    const handleNavigateToCreateTimer = () => {
-        navigation.navigate("Your Timers"); // Replace "CreateTimerScreen" with the name of the screen you want to navigate to
-      };
+export default function EmptyState({ navigation }) {
+  const handleNavigateToCreateTimer = () => {
+    navigation.navigate("Your Timers"); // Replace "CreateTimerScreen" with the name of the screen you want to navigate to
+  };
 
   return (
     <View style={styles.container}>
@@ -16,6 +17,7 @@ export default function EmptyState({navigation}) {
         color={Colors.DARK_GRAY}
       />
       <Text style={styles.text}>No active timers </Text>
+
       <TouchableOpacity onPress={handleNavigateToCreateTimer}>
         <Text style={styles.addTimerNav}>+ select a timer</Text>
       </TouchableOpacity>
@@ -34,10 +36,10 @@ const styles = StyleSheet.create({
     fontFamily: "Regular",
     fontSize: 30,
   },
-  addTimerNav:{
-    marginTop: 60,
-    fontFamily: 'Light',
+  addTimerNav: {
+    marginTop: 30,
+    fontFamily: "Light",
     color: Colors.LIGHT,
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
